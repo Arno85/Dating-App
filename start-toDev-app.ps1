6aa3e107435b38a8b2da5367f333fdfffb4d9334
Arnaud <arnaud.martin85@gmail.com>
2019-04-13 20:10:02 -0400

    angular setup

--- start-toDev-app.ps1
+++
@@ -1,6 +0,0 @@
-$appName = "DatingApp"
-$FrontEndCmd = "/c cd $appName-SPA && ng serve"
-$BackEndCmd = "/c cd $appName.API && dotnet watch run"
-
-Start cmd -Argument $FrontEndCmd -WindowStyle Minimized
-Start cmd -Argument $BackEndCmd -WindowStyle Minimized
