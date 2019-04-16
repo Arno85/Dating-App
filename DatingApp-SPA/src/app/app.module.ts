14efcaecfd9804df4d8c9fc70d11e7fb6e90eec7
Arnaud <arnaud.martin85@gmail.com>
2019-04-16 18:10:02 -0400

    gulp tasks + cleanup

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ValueComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
